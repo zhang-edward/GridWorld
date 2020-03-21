20dd6f0d003c31327f11693f22a900f63ea3e68e
Edward Zhang <edwardz123@gmail.com>
2020-03-21 10:07:43 -0700

    Remove time variable from Behavior

--- StoryBox/Assets/Scripts/Behaviors/Behavior.cs
+++ StoryBox/Assets/Scripts/Behaviors/Behavior.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public abstract class Behavior : MonoBehaviour {
 
-	public int time = 1;
-
 	public virtual void Init() {}
 	public abstract NodeStatus Act(Entity entity, Memory memory);
 }
