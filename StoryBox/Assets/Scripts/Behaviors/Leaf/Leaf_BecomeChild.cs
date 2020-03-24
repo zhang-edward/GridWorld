d70d6ea78e3985e9e74b6ec0b602cc0918b7483e
Edward Zhang <edwardz123@gmail.com>
2020-03-24 02:20:03 -0700

    Add BecomeChild leaf node

---
+++ StoryBox/Assets/Scripts/Behaviors/Leaf/Leaf_BecomeChild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf_BecomeChild : Behavior {
+
+	[Header("Target Entity")]
+	[Tooltip("Entity")]
+	public string targetEntity;
+
+    [Header("Write Keys to target")]
+	public string targetWriteKey;
+
+	public override NodeStatus Act(Entity entity, Memory memory) {
+		Entity target = (Entity)memory[targetEntity];
+		List<Entity> list = target.memory[targetWriteKey] as List<Entity>;
+		if (list == null)
+			list = new List<Entity>();
+		list.Add(entity);
+		target.memory[targetWriteKey] = list;
+
+		return NodeStatus.Success;
+	}
+}
