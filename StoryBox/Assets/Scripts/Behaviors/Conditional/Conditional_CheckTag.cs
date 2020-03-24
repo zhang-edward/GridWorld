edbe475af4f23693f721b148a22a127decaa3abf
Edward Zhang <edwardz123@gmail.com>
2020-03-24 02:18:37 -0700

    Add CheckTag conditional

---
+++ StoryBox/Assets/Scripts/Behaviors/Conditional/Conditional_CheckTag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditional_CheckTag : Behavior {
+
+	public List<string> tagValues;
+
+	[Header("Read Keys")]
+	[Tooltip("Entity")]
+	public string entityKey;
+
+	public override NodeStatus Act(Entity entity, Memory memory) {
+		Entity target = (Entity)memory[entityKey];
+
+		foreach (string tagValue in tagValues) {
+			if (!target.tags.Contains(tagValue))
+				return NodeStatus.Failure;
+		}
+		return NodeStatus.Success;
+	}
+}
